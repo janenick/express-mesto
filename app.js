3b8adb9ab0eeb8292747c46b97ac0c1838c84201
Evgeniya Nikitina <janebornick@yandex.ru>
2020-11-01 12:17:19 +0300

    feat: add rout users

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const path = require('path');
 
-const usersRouter = require('./routers/users.js');
+const usersRouter = require('./routes/users.js');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public'))); // теперь клиент имеет доступ только к публичным файлам 
+app.use(express.static(path.join(__dirname, 'public'))); // теперь клиент имеет доступ только к публичным файлам
 
 app.use('/', usersRouter);
 
 app.use('*', (req, res) => {
-  res.status(404).send({ error: 'Нет пути' });
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`App listening on port ${PORT}`);
 });
- 
---
+++ controllers/users.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const readFile = require('../utils/read-file');
+
+const pathToData = path.join(__dirname, '..', 'users.json');
+
+module.exports.getUsers = (req, res) => {
+  readFile(pathToData)
+    .then(data => res.send(data))
+    .catch((err) => {
+      res.status(404).send({ error: 'Запрашиваемый ресурс не найден' })
+    });
+};
+
+module.exports.getUser = (req, res) => {
+  const { id } = req.params;
+  readFile(pathToData)
+    .then(data => {
+      const user = data.users.find(item => {
+        return item._id === id;
+      });
+      if (!user) {
+        res.status(404).send({ error: 'Нет пользователя с таким id' })
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(404).send({ error: 'Запрашиваемый ресурс не найден' })
+    });
+  /*if (!users[id]) {
+  res.send({ error: 'Такого пользователя нет' });
+  return;
+}
+
+res.send(users[id]);*/
+};
