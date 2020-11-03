8f35a67fcc9adced656dfcc443b9ff4b6109a2f1
Evgeniya Nikitina <janebornick@yandex.ru>
2020-11-03 11:46:02 +0300

    fix: review errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,4 +1,3 @@
-/* eslint-disable linebreak-style */
 const path = require('path');
 
 const readFile = require('../utils/read-file');
@@ -7,8 +6,5 @@ const pathToData = path.join(__dirname, '..', 'data', 'cards.json');
 
 module.exports.getCards = (req, res) => {
   readFile(pathToData)
-    .then(data => res.send(data))
-    .catch((err) => {
-      res.status(404).send({ error: 'cards Запрашиваемый ресурс не найден' });
-    });
+    .then((data) => res.send(data));
 };
--- controllers/users.js
+++ controllers/users.js
@@ -6,23 +6,18 @@ const pathToData = path.join(__dirname, '..', 'data', 'users.json');
 
 module.exports.getUsers = (req, res) => {
   readFile(pathToData)
-    .then((data) => res.status(200).send(data))
-    .catch(() => {
-      res.status(500).send({ error: 'Запрашиваемый файл не найден' })
-    });
+    .then((data) => res.status(200).send(data));
 };
 
 module.exports.getUser = (req, res) => {
   const { id } = req.params;
   readFile(pathToData)
     .then((data) => {
-      const user = data.users.find((item) => item._id === id);
+      const user = data.find((item) => item._id === id);
       if (!user) {
-        res.status(404).send({ error: 'Нет пользователя с таким id' });
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+      } else {
+        res.status(200).send(user);
       }
-      res.status(200).send(user);
-    })
-    .catch(() => {
-      res.status(500).send({ error: 'Запрашиваемый файл не найден' });
     });
 };
